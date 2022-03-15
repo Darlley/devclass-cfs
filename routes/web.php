dd2f0adde65f0113900954759df39b05667fb182
Darlley <systembrtt@gmail.com>
2022-03-15 17:33:34 -0400

    aula 30

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+// 
+// Route::get('/user/{id}/{name}/{age}', function($id,$name,$age){
+//     dd("User: $id ($name), $age yers");
+// })->where(["id" => "[0-9]+", "name" => "[A-z]+", "age" => "[0-9]+"]);
+
+Route::get('/user/{id}/{name}/{age}', function($id,$name,$age){
+    dd("User: $id ($name), $age yers");
+})->whereNumber('id')->whereAlpha('name')->whereNumber('age');
